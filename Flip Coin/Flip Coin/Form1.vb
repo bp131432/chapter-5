055b4eb725f1b3a74d84416e1ee532160d5fbe92
Bailey <33033378+bp131432@users.noreply.github.com>
2017-12-04 11:31:50 -0600

    Updated Race

--- Flip Coin/Flip Coin/Form1.vb
+++ Flip Coin/Flip Coin/Form1.vb
@@ -1,4 +1,15 @@
 ﻿Public Class Form1
+    Sub tails()
+        picTails.Visible = True
+        picHeads.Visible = False
+        tailTotal = 1 + tailTotal
+    End Sub
+    Sub heads()
+        picHeads.Visible = True
+        picTails.Visible = False
+        headTotal = 1 + headTotal
+    End Sub
+
     Dim tailTotal As Integer = 0
     Dim headTotal As Integer = 0
     Private Sub btnToss_Click(sender As Object, e As EventArgs) Handles btnToss.Click
@@ -6,20 +17,16 @@
         Dim Score As Integer = 0
         Dim rand As New Random
 
-            intSideUp = rand.Next(2)
-            If intSideUp = 0 Then
-                picTails.Visible = True
-                picHeads.Visible = False
-            tailTotal = 1 + tailTotal
-            Else
-                picHeads.Visible = True
-                picTails.Visible = False
-            headTotal = 1 + headTotal
+        intSideUp = rand.Next(2)
+        If intSideUp = 0 Then
+            tails()
+        Else
+            heads()
 
-                lblNumberOfHeads.Text = headTotal.ToString("n")
-                lblNumberOfTails.Text = tailTotal.ToString("n")
+            lblNumberOfHeads.Text = headTotal.ToString("n")
+            lblNumberOfTails.Text = tailTotal.ToString("n")
 
-            End If
+        End If
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
--- Race/Race/Form1.vb
+++ Race/Race/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
-    Dim chickScore As Integer = 0
-    Dim puppyScore As Integer = 0
-    Dim goatScore As Integer = 0
+    Dim chickScore As Integer
+    Dim puppyScore As Integer
+    Dim goatScore As Integer
     Private Sub btnRace_Click(sender As Object, e As EventArgs) Handles btnRace.Click
         Dim rand As New Random
         Dim intCount1 As Integer
@@ -14,23 +14,31 @@
         For intCount2 = 16 To rand.Next(200) + 400
             picPuppy.Left = intCount2
         Next
-        For intCount3 = 16 To rand.Next(200) + 400
+        For intCount3 = 16 To rand.Next(200) + 0
             picGoat.Left = intCount3
         Next
 
-        If chickScore < puppyScore And goatScore Then
-            lblChick.Text += 1
-        ElseIf puppyScore < chickScore And goatScore Then
-            lblPuppy.Text += 1
-        ElseIf goatScore < puppyScore And chickScore Then
-            lblGoat.Text += 1
+        If intCount1 > intCount2 And intCount3 Then
+            chickScore = 1 + chickScore
+        ElseIf intCount2 > intCount1 And intCount3 Then
+            puppyScore = 1 + puppyScore
+        ElseIf intCount3 > intCount2 And intCount1 Then
+            goatScore = 1 + goatScore
         End If
+
+        lblChick.Text = chickScore
+        lblGoat.Text = goatScore
+        lblPuppy.Text = puppyScore
     End Sub
 
     Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
         picChick.Left = 16
         picGoat.Left = 16
         picPuppy.Left = 16
+
+        lblChick.Text = ""
+        lblGoat.Text = ""
+        lblPuppy.Text = ""
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
