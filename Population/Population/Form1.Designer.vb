155705e980cf45c296487eec288eccd7014d66b2
Bailey <33033378+bp131432@users.noreply.github.com>
2017-12-07 11:06:26 -0600

    Created population

---
+++ Population/Population/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.cboDays = New System.Windows.Forms.ComboBox()
+        Me.txtIncrease = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(57, 278)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 0
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(57, 97)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(202, 173)
+        Me.lstOutput.TabIndex = 1
+        '
+        'cboDays
+        '
+        Me.cboDays.FormattingEnabled = True
+        Me.cboDays.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.cboDays.Location = New System.Drawing.Point(151, 70)
+        Me.cboDays.Name = "cboDays"
+        Me.cboDays.Size = New System.Drawing.Size(121, 21)
+        Me.cboDays.TabIndex = 2
+        '
+        'txtIncrease
+        '
+        Me.txtIncrease.Location = New System.Drawing.Point(151, 21)
+        Me.txtIncrease.Name = "txtIncrease"
+        Me.txtIncrease.Size = New System.Drawing.Size(100, 20)
+        Me.txtIncrease.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Enter the increase:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Select number of days:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(197, 278)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(325, 337)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtIncrease)
+        Me.Controls.Add(Me.cboDays)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents cboDays As System.Windows.Forms.ComboBox
+    Friend WithEvents txtIncrease As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Population/Population/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim intCount As Integer
+        Dim dblTotal As Double
+
+        'Calculate population, and display output
+        lstOutput.Items.Add("Day Approximate Population")
+        lstOutput.Items.Add("-----------------------------------")
+        dblTotal = 2
+        intCount = 1
+
+
+        lstOutput.Items.Add(intCount.ToString + " " + dblTotal.ToString)
+
+        For intCount = intCount + 1 To cboDays.Text
+            dblTotal = ((CDbl(txtIncrease.Text) * dblTotal) / 100) + dblTotal
+            lstOutput.Items.Add(intCount.ToString + " " + dblTotal.ToString)
+        Next
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
